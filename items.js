d40d0961e1d58a1e1104642194b2c150bcfca99d
masande <gontyelenimasande@gmail.com>
2021-03-09 14:08:34 +0200

    made some changes

--- items.js
+++ items.js
@@ -68,6 +68,108 @@ function addTocart() {
   JSON.parse(window.localStorage.getItem("product"));
 }
 
+addTocart();
+
 function toggleModal() {
   document.getElementById("modal").classList.toggle("active");
 }
+
+// // Get data from localStorage
+// let cart = JSON.parse(localStorage.getItem("cart"));
+// cart ? cart : (cart = []);
+
+// function updateCartLink() {
+//   document.getElementById("cart-link").innerHTML = `Cart(${cart.length})`;
+// }
+// updateCartLink();
+
+// // Check if cart content is available
+
+// let cartContent = document.getElementById("cart-content");
+// console.log(cartContent);
+
+// function renderCart() {
+//   // Get data from localStorage
+//   let cart = JSON.parse(localStorage.getItem("cart"));
+//   cartContent.innerHTML = "";
+//   if (cartContent) {
+//     cartContent.innerHTML = `
+//             <h1 style="text-align:center;">Cart</h1>
+//             <table style="text-align: left">
+//                 <thead style="background: #bebebe">
+//                     <tr class="test">
+//                         <th class=""imgcart">Image</th>
+//                         <th class="namecart">Name</th>
+//                         <th class="catcart">Category</th>
+//                         <th class="pricecart">Price</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     ${renderCartItems()}
+//                 </tbody>
+//             </table>
+//         `;
+//   }
+//   console.log("Cart Rendered");
+// }
+
+// renderCart();
+
+// function renderCartItems() {
+//   console.log("Rendering Cart Items");
+//   let content = "";
+//   cart.forEach((item) => {
+//     content += `
+//             <tr>
+//                 <td><img src=${item.image} alt=${item.name}/></td>
+//                 <td>${item.name}</td>
+//                 <td>${item.category}</td>
+//                 <td>${item.price}</td>
+//                 <td class="removeBtn"><button type="button" onclick="remove(${item.id})"> Remove from cart </button></td>
+//             </tr>
+
+//         `;
+//   });
+
+//   return content;
+// }
+
+// function add() {
+//   let cart = JSON.parse(localStorage.getItem("cart"));
+//   cart ? cart : (cart = []);
+//   cart.push(selectedProduct);
+//   console.log(cart);
+//   document.getElementById("cart-link").innerHTML = `Cart(${cart.length})`;
+//   localStorage.setItem("cart", JSON.stringify(cart));
+// }
+
+// function remove(id) {
+//   let cart = JSON.parse(localStorage.getItem("cart"));
+//   // cart = JSON.parse(cartItem);
+//   console.log(cart);
+
+//   let cartMinusItem = cart.filter((item) => {
+//     return item.id !== id;
+//   });
+
+//   localStorage.setItem("cart", JSON.stringify(cartMinusItem));
+
+//   console.log("Removing item from cart");
+//   renderCart();
+//   window.location.href = "./cart.html";
+// }
+
+// function clearCart() {
+//   localStorage.removeItem("cart"); //clear local storage data
+// }
+
+// function createCart() {
+//   let inCart;
+
+//   let cart = localStorage.getItem("cart");
+//   console.log("Whats in your cart?", JSON.parse(cart));
+// }
+
+// function showCart() {
+//   let added = document.getElementById("cartItem");
+// }
